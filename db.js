a9090833a4216ebb7cc76bf3c9866a8b06736703
adnanrahic <ado.raha198@gmail.com>
2018-05-24 13:40:15 +0200

    added atlas db

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/bita4students-nodejs-api');
+
+// mongoose.connect('mongodb://burch:burch123@cluster0-shard-00-00-ttbct.mongodb.net:27017,cluster0-shard-00-01-ttbct.mongodb.net:27017,cluster0-shard-00-02-ttbct.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
